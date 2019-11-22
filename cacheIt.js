4e114deac649a703c5a9611cdaeac09151a467e7
Sarthak <sarthak.whenever@gmail.com>
2019-11-22 18:29:39 +0545

    Font size of cache link show page increased with a h3 tag

--- cacheIt.js
+++ cacheIt.js
@@ -41,7 +41,7 @@ async function cacheIt(request, response) {
             log(written[1]);
             log(writtenJSON[1]);
         }else {
-            dataRes = `Successfully cached the link. Your cacheLink is at this link <a href="/caches/${hashName}/">/caches/${hashName}/</a>`;
+    dataRes = `<h3>Successfully cached the link. Your cacheLink is at this link <a href="/caches/${hashName}/">/caches/${hashName}/</a><h3>`;
         }
         response.writeHead(resStatCode, {
             "Content-type": "text/html; charset=utf-8"
